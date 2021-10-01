11739b4efeb8e2416b9e0d0362929aee77dd97ca
j-ortiz-ici <89707086+j-ortiz-ici@users.noreply.github.com>
2021-10-01 17:28:25 -0300

    Delete Login.java

--- .idea/src/main/java/Login.java
+++
@@ -1,8 +0,0 @@
-import java.io.IOException;
-
-public class Login {
-    public static void LoginCoso() throws IOException {
-        textFile IDPass = new textFile();
-        loginPage lp = new loginPage(IDPass.archivo());
-    }
-}
